f62c31e8379245e8afa049313dc5bd33a9431740
Garduno Calderon, Juan P <juan.p.garduno.calderon@intel.com>
2025-04-21 09:50:30 -0600

    feat: add a github workflow for selenium tests
    Run selenium tests with behave as part of GitHub actions workflows.

--- selenium/steps/search.py
+++ selenium/steps/search.py
@@ -3,11 +3,8 @@
 """
 System Test example using Behavior-Driven Development (BDD) with Behave (Gherkin) and Selenium.
 """
-import random
-import time
-
 from behave import given, then, when  # pylint: disable=no-name-in-module
-from chromedriver_py import binary_path
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -19,10 +16,10 @@ from selenium import webdriver
 @given("I am on the Google homepage")
 def open_browser(context):
     """Opens Google in Chrome."""
-    svc = webdriver.ChromeService(executable_path=binary_path)
-    context.driver = webdriver.Chrome(service=svc)
+    options = Options()
+    options.add_argument("--headless")
+    context.driver = webdriver.Chrome(options=options)
     context.driver.get("https://www.google.com")
-    time.sleep(random.uniform(3, 5))
 
 
 @when('I search for "{query}"')
@@ -30,9 +27,7 @@ def search(context, query):
     """Searches something in Google."""
     search_box = context.driver.find_element("id", "APjFqb")
     search_box.send_keys(query)
-    time.sleep(random.uniform(3, 5))
     search_box.send_keys(Keys.RETURN)
-    time.sleep(random.uniform(3, 5))
     delay = 5  # seconds
     wait = WebDriverWait(context.driver, delay)
     wait.until(EC.presence_of_element_located((By.ID, "rcnt")))
